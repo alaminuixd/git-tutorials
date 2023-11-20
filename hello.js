0a144f51827018281c1983e798c5597292df3468
alaminuixd <alaminuixd@gmail.com>
2023-11-20 18:27:02 +0600

    class greet construction with one method startGreet

---
+++ hello.js
@@ -0,0 +1,15 @@
+class greet {
+    constructor(name, age, greetType, greeting){
+        this.name = name;
+        this.age = age;
+        this.greetType = greetType;
+        this.greeting = greeting;
+    }
+    startGreet = function(){
+        const gText = `My Name is ${this.name} and I am ${this.age} years old. I would like to ${this.greeting} you on account of the ${this.greetType}.`;
+        return gText;
+    }
+}
+
+const g1 = new greet('Alice', 34, 'Eid Ul Fitr', 'Eid mubarak');
+console.log(g1.startGreet());
