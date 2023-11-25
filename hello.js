351e2299676ba7e43f03f6be0ce67b3dc7c0bce4
alaminuixd <alaminuixd@gmail.com>
2023-11-25 22:44:48 +0600

    hello.js & thied.txt update

--- hello.js
+++ hello.js
@@ -9,9 +9,19 @@ class greet {
         const gText = `My Name is ${this.name} and I am ${this.age} years old. I would like to ${this.greeting} you on account of the ${this.greetType}.`;
         return gText;
     }
+    sayHobies = function(obj){
+        const gText = `I like playing ${obj.one}, ${obj.two}, and ${obj.three}`;
+        return gText;
+    }
+}
+const myObj = {
+    one: 'football',
+    two: 'fishing',
+    three: 'reading story book'
 }
 
 const g1 = new greet('Alice', 34, 'Eid Ul Fitr', 'Eid mubarak');
 console.log(g1.startGreet());
 
 console.log(`--------------------------------------`);
+console.log(g1.sayHobies(myObj));
