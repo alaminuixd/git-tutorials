cd6c373f516a4fef05066db3107507d9e050e1f1
alaminuixd <alaminuixd@gmail.com>
2023-11-26 00:28:10 +0600

    new branchC commit

--- hello.js
+++ hello.js
@@ -5,29 +5,6 @@ class greet {
         this.greetType = greetType;
         this.greeting = greeting;
     }
-    startGreet = function(){
-        const gText = `My Name is ${this.name} and I am ${this.age} years old. I would like to ${this.greeting} you on account of the ${this.greetType}.`;
-        return gText;
-    }
-    sayHobies = function(obj){
-        const gText = `I like playing ${obj.one}, ${obj.two}, and ${obj.three}`;
-        return gText;
-    }
-    openDoor = function(){
-        return `I am opening a door`;
-    }
-    eatMeal = function(){
-        return `I am eating my meal`;
-    }
-}
-const myObj = {
-    one: 'football',
-    two: 'fishing',
-    three: 'reading story book'
 }
 
 const g1 = new greet('Alice', 34, 'Eid Ul Fitr', 'Eid mubarak');
-console.log(g1.startGreet());
-
-console.log(`--------------------------------------`);
-console.log(g1.sayHobies(myObj));
