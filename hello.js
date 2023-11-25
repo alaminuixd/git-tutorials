7293b7a89ec974753bb8ee75f07ed2ef7567d270
alaminuixd <alaminuixd@gmail.com>
2023-11-26 00:31:54 +0600

    modified hello.js

--- hello.js
+++ hello.js
@@ -5,6 +5,16 @@ class greet {
         this.greetType = greetType;
         this.greeting = greeting;
     }
+    startGreet = function(){
+        const gText = `My Name is ${this.name} and I am ${this.age} years old. I would like to ${this.greeting} you on account of the ${this.greetType}.`;
+        return gText;
+    }
+    sayHobies = function(obj){
+        const gText = `I like playing ${obj.one}, ${obj.two}, and ${obj.three}`;
+        return gText;
+    }
 }
 
+
 const g1 = new greet('Alice', 34, 'Eid Ul Fitr', 'Eid mubarak');
+console.log(g1.startGreet());
